ed2dea2a83aebc6ba733cdd1050d7048279c67c4
RoyerSMG <royersteven2004@gmail.com>
2026-05-24 17:35:37 -0500

    cambio en variables operador

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
               <circle cx="12" cy="8" r="4"/>
               <path d="M4 20c0-4 3.6-7 8-7s8 3 8 7"/>
             </svg>
-            <span className="text-white/80 text-xs">{operador}</span>
+            <span className="text-white/80 text-xs">{operador?.nombre}</span>
           </div>
 
           <button
